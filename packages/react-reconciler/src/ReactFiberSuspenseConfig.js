e8395579a582a33940713499e11bc05633381147
ahungrynoob <dxd_sjtu@outlook.com>
2019-08-06 09:49:25 +0800

    temp: ready to updateContainer

---
+++ packages/react-reconciler/src/ReactFiberSuspenseConfig.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import ReactSharedInternals from 'shared/ReactSharedInternals';
+
+const {ReactCurrentBatchConfig} = ReactSharedInternals;
+
+export function requestCurrentSuspenseConfig(){
+  return ReactCurrentBatchConfig.suspense;
+}
